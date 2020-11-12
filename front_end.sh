5ca2af9907905999a4aeff246c042c494d8069b1
japsasir <japsasir@gmail.com>
2020-11-12 23:19:46 +0100

    Instalación de phpMyAdmin
    -La conexión a PhP my admin es posible
    -Error de autorización para acceder con root o lamp_user
    
    Código al pié: mysqli::real_connect(): (HY000/2002): No such file or directory

--- front_end.sh
+++ front_end.sh
@@ -7,7 +7,7 @@ HTTPASSWD_USER=usuario
 HTTPASSWD_PASSWD=usuario
 
 # IP del Servidor MySQL que debermos cambiar a menudo.
-IPPRIVADA=172.31.88.192
+IPPRIVADAMYSQL=172.31.88.192
 
 # ------------------------------------------------------------------------------ Instalación de Apache ------------------------------------------------------------------------------ 
 # Habilitamos el modo de shell para mostrar los comandos que se ejecutan
@@ -30,7 +30,7 @@ git clone https://github.com/josejuansanchez/iaw-practica-lamp
 mv /var/www/html/iaw-practica-lamp/src/*  /var/www/html/
 
 # Configuramos el archivo php de la aplicacion. En https://linuxhint.com/bash_sed_examples/ podemos leer sobre las especificaciones del comando sed y el operador -i, que reemplazarán la línea. Ojo a las comillas, tienen que ser dobles.
-sed -i "s/localhost/$IPPRIVADA/" /var/www/html/config.php
+sed -i "s/localhost/$IPPRIVADAMYSQL/" /var/www/html/config.php
 
 # Eliminamos el archivo Index.html de apache
 rm -rf /var/www/html/index.html
@@ -59,22 +59,37 @@ mkdir /var/www/html/stats
 nohup goaccess /var/log/apache2/access.log -o /var/www/html/stats/index.html --log-format=COMBINED --real-time-html &
 htpasswd -c -b $HTTPASSWD_DIR/.htpasswd $HTTPASSWD_USER $HTTPASSWD_PASSWD
 
+# Instalamos phpMyAdmin #
+# Nos situamos en el directorio de usuario
+cd $HTTPASSWD_DIR
+
+# Nos aseguramos que no existe ya el directorio phpMyAdmin-5.0.4-all-languages.zip
+rm -rf phpMyAdmin-5.0.4-all-languages.zip
+
+# Descargamos el paquete phpMyAdmin 
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.zip
+
 # Instalamos unzip
 apt install unzip -y
-# Instalación de Phpmyadmin
-cd /home/ubuntu
-rm -rf phpMyAdmin-5.0.4-all-lenguages.zip
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-lenguages.zip
-# Descomprimimos 
-nzip phpMyAdmin-5.0.4-all-lenguages.zip
-# Borramos el archivo .zip
-rm -rf phpMyAdmin-5.0.4-all-lenguages.zip
-# Movemos la carpeta al directorio
-mv phpMyAdmin-5.0.4-all-lenguages /var/www/html/phpmyadmin
-# Configuaramos el archivo config.sample.inc.php
+
+# Descomprimimos phpMyAdmin-5.0.4-all-languages.zip
+unzip phpMyAdmin-5.0.4-all-languages.zip
+
+# Borramos el archivo zip
+rm -rf phpMyAdmin-5.0.4-all-languages.zip
+
+# Movemos el directorio de phpMyAdmin al directorio /var/www/html
+mv phpMyAdmin-5.0.4-all-languages/ /var/www/html/phpmyadmin
+
+# Configuramos el archivo config.inc.php de phpMyAdmin #
+# Nos situamos en el directorio /var/www/html/phpmyadmin
 cd /var/www/html/phpmyadmin
+
+# Cambiamos el nombre del archivo 
 mv config.sample.inc.php config.inc.php
-sed -i "s/localhost/$IPPRIVADA/" /var/www/html/config.inc.php
+
+# Modificamos el archivo config.inc.php para que podamos conectarnos a la ip privada del backend
+sed -i "s/localhost/$IP_PRIVADA_MYSQL/" /var/www/html/phpmyadmin/config.inc.php
 
 # Cambiamos permisos de /var/www/html
 cd /var/www/html
